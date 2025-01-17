4de421c69f338be830b6cae679e671a3e79af6f1
Rinat <rinenik3105@gmail.com>
2025-01-18 01:32:33 +0300

    ru locale forr statuses

--- app/models/reading_status.rb
+++ app/models/reading_status.rb
@@ -1,7 +1,7 @@
 class ReadingStatus < ApplicationRecord
   belongs_to :user
 
-  STATUSES = ['not read', 'reading', 'read', 'want to read'].freeze
+  STATUSES = ['читаю', 'прочитал', 'хочу прочитать'].freeze
 
   validates :isbn, presence: true
   validates :status, inclusion: { in: STATUSES, message: "Invalid status. Available statuses: #{STATUSES.join(', ')}" }
